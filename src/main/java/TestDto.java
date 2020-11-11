f38b9041309bd4de42d640b7b11fa7ca4581a4d8
Artem Kazlanzhy <akazlanzhy@provectus.com>
2020-11-11 17:26:55 +0200

    Initial commit

---
+++ src/main/java/TestDto.java
@@ -0,0 +1,14 @@
+public class TestDto {
+    private String name;
+
+    public TestDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
